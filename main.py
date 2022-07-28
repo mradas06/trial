d7cea7be1b5b5ee144603c217d084d12ebe6dca4
Çağrı Adaş <58921635+mradas06@users.noreply.github.com>
2022-07-28 16:16:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
+
 import pyvisa
 
 
+def readDeviceName(self):
+    self.write(b"IDN?\n")
+    deviceName = self.read()
+    print(deviceName)
+    return deviceName
 class hmp4040():
 
-    def __init__(self, pyvisa_instr):
-        self.hmp4040 = pyvisa_instr
+    def __init__(self,resource) -> None:
+        self.resource = resource
+        pass
 
-    def write(str):
+    def write(self):
         return self.resource.write(str)
 
-    def read(str):
-        return self.resource.read(str)  
-
-    def readDeviceName(self):
-        self.write(b"IDN?\n")
-        deviceName = self.read()
-        print(deviceName)
-        return deviceName
+    def read(self):
+        return self.resource.read(str)
 
     def setVoltage(self, voltage, channel):
         return self.write(b"INST OUT" + str(channel).encode() + b"\nVOLT " + str(voltage).encode() + b"\n")
@@ -27,22 +28,22 @@ class hmp4040():
     def getVoltage(self, channel):
         self.write(b"INST OUT" + str(channel).encode() + b"\nMEAS:VOLT?\n")
         voltage = self.read()
-        return float(voltage.strip())
+        return float(voltage)
 
     def getCurrent(self, channel):
         self.write(b"INST OUT" + str(channel).encode() + b"\nMEAS:CURR?\n")
         current = self.read()
-        return float(current.strip())
+        return float(current)
 
     def getTargetVoltage(self, channel):
         self.write(b"INST OUT" + str(channel).encode() + b"\nVOLT?\n")      #limit#
         voltage = self.read()
-        return float(voltage.strip())
+        return float(voltage)
 
     def getTargetCurrent(self, channel):
         self.write(b"INST OUT" + str(channel).encode() + b"\nCURR?\n")      #limit#
         current = self.read()
-        return float(current.strip())
+        return float(current)
 
     def outputOn(self, channel):
         self.write(b"INST OUT" + str(channel).encode() + b"\nOUTP ON\n")    #channel on#
