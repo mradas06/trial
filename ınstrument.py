90aa0d27a141d32dfd46df78afd4ab156bfb37f8
Çağrı Adaş <58921635+mradas06@users.noreply.github.com>
2022-07-29 11:35:48 +0300

    Update ınstrument.py

--- ınstrument.py
+++ ınstrument.py
@@ -6,7 +6,7 @@ from main import*
 rm = pyvisa.ResourceManager() # List all connected resources
 print("Resources detected\n{}\n".format(rm.list_resources()))
 rID = rm.open_resource('ASRL6::INSTR')
-hmp4040 = hmp4040(resource=rID)
+hmp4040 = Hmp4040(resource=rID)
 
 rID.write('*RST')
 time.sleep(2)
