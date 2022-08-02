8574ae2a8b50f7c89422bf917dd2cdb1dd5461c7
Çağrı Adaş <58921635+mradas06@users.noreply.github.com>
2022-08-02 15:06:23 +0300

    Update ınstrument.py
    02.08 güncellemesi
    Son halini almış bulunuyor isteğe göre senaryolar veya istenilen durumlar eklenebilir.
    Ayrıca GUI üzerine çalışıyorum kendime simülasyon guisi oluşturdum ordan veri çekmeye çalışıyorum.

--- ınstrument.py
+++ ınstrument.py
@@ -5,8 +5,11 @@ from main import*
 
 rm = pyvisa.ResourceManager() # List all connected resources
 print("Resources detected\n{}\n".format(rm.list_resources()))
-rID = rm.open_resource('ASRL6::INSTR')
-hmp4040 = Hmp4040(resource=rID)
+rID = rm.open_resource(rm.list_resources()[0])
+print('Power supply detected=> ' + rID.query('*IDN?')) 
+hmp4040 = HMP4040(resource=rID)
 
 rID.write('*RST')
 time.sleep(2)
+
+rID.close()
